f17a3556a212df247683ada629c0973986381d07
Davi do Egito <66625452+zucomania@users.noreply.github.com>
2024-10-24 08:53:45 -0300

    Translating

--- EDA1/Calculadora.c
+++ EDA1/Calculadora.c
@@ -19,7 +19,7 @@ int main()
                 scanf("%d", &sum_1);
                 printf("Type the second number to be added\n");
                 scanf("%d", &sum_2);
-                printf("O resultado foi %d\n", sum_1 + sum_2);
+                printf("The result was equal to: %d\n", sum_1 + sum_2);
             } else if (number == 2) {
                 int subt_1;
                 int subt_2;
@@ -28,7 +28,7 @@ int main()
                 scanf("%d", &subt_1);
                 printf("Type the second number to be added");
                 scanf("%d", &subt_2);
-                printf("O resultado foi %d\n", subt_1 - subt_2); 
+                printf("The result was equal to: %d\n", subt_1 - subt_2); 
             } else if (number == 3) {
                 int mult_1;
                 int mult_2;
@@ -37,7 +37,7 @@ int main()
                 scanf("%d", &mult_1);
                 printf("Type the second number to be added");
                 scanf("%d", &mult_2);
-                printf("O resultado foi %d\n", mult_1 * mult_2); 
+                printf("The result was equal to: %d\n", mult_1 * mult_2); 
             } else if (number == 4) {
                 int div_1;
                 int div_2;
@@ -46,7 +46,7 @@ int main()
                 scanf("%d", &div_1);
                 printf("Type the second number to be added");
                 scanf("%d", &div_2);
-                printf("O resultado foi %d\n", div_1 / div_2); 
+                printf("The result was equal to: %d\n", div_1 / div_2); 
             }
     } while (number <= 4);
     return 0;
