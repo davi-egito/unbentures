318ef8ab9f680b6cea7023c3daa6ed98e0401614
Davi do Egito <66625452+zucomania@users.noreply.github.com>
2024-11-29 07:29:28 -0300

    Removing comments from MIPS code

--- FAC/ola_mundo.asm
+++ FAC/ola_mundo.asm
@@ -6,8 +6,8 @@ ola: .asciiz "Ola Mundo\n"
 .globl main
 
 main:
-  li $v0, 4 #4 é o código da syscall para imprimir string
-  la $a0, ola #o endereço de ola lá em .data será carregado em a0, que é um argumento
+  li $v0, 4 
+  la $a0, ola 
   syscall
-  li $v0, 10 #10 é o código da syscall para encerrar o programa
+  li $v0, 10 
   syscall
