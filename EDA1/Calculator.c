3fce647ff1d0cd94c5de7a41366201e90695739d
Davi do Egito <66625452+zucomania@users.noreply.github.com>
2024-12-07 22:19:22 -0300

    added do/while

--- EDA1/Calculator.c
+++ EDA1/Calculator.c
@@ -2,52 +2,34 @@
 Here's a calculator to help you with simples tasks. You might insert two numbers and it will help ye with the result.
 *******************************************************************************/
 
-#include <stdio.h>
+//It must have a menu and use can randomize the number of items.
 
-int main()
-{
-    int number;
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char produto[10][15] = {"Arroz", "Feijao", "Macarrao", "Carne", "Frango", "Peixe", "Oleo", "Sal", "Acucar", "Cafe"};
+    int indice, i, quant_final, escolha; //o valor inteiro corresponde à posição do produto
     do {
-        printf("\n*** Hello, and welcome to this simples task calculator! ***\n");
-        printf("Type 1 to sum, 2 to subtract, 3 to multiply and 4 to divide!\n");
-        scanf(" %d", &number);
-            if (number == 1) {
-                int sum_1;
-                int sum_2;
-                int sum_total;
-                printf("Type the first number to be added\n");
-                scanf("%d", &sum_1);
-                printf("Type the second number to be added\n");
-                scanf("%d", &sum_2);
-                printf("The result was equal to: %d\n", sum_1 + sum_2);
-            } else if (number == 2) {
-                int subt_1;
-                int subt_2;
-                int subt_total;
-                printf("Type the first number to be added\n");
-                scanf("%d", &subt_1);
-                printf("Type the second number to be added");
-                scanf("%d", &subt_2);
-                printf("The result was equal to: %d\n", subt_1 - subt_2); 
-            } else if (number == 3) {
-                int mult_1;
-                int mult_2;
-                int mult_total;
-                printf("Type the first number to be added\n");
-                scanf("%d", &mult_1);
-                printf("Type the second number to be added");
-                scanf("%d", &mult_2);
-                printf("The result was equal to: %d\n", mult_1 * mult_2); 
-            } else if (number == 4) {
-                int div_1;
-                int div_2;
-                int div_total;
-                printf("Type the first number to be added");
-                scanf("%d", &div_1);
-                printf("Type the second number to be added");
-                scanf("%d", &div_2);
-                printf("The result was equal to: %d\n", div_1 / div_2); 
-            }
-    } while (number <= 4);
+    printf("Você deseja comprar ou adicionar produtos?\nDigite 1 para comprar e 2 para adicionar produtos: ");
+    scanf("%d", &escolha);
+    if (escolha == 1) {
+        printf("Quantos produtos você quer comprar?\n");
+        scanf("%d", &quant_final);
+        for (i = 0; i < quant_final; i++) {
+            indice = rand() % 10;
+            printf("Produto %d: %s\n", i+1, produto[indice]);
+        }
+    } else if (escolha == 2) {
+        printf("Quantos produtos você quer adicionar?\n");
+        scanf("%d", &quant_final);
+        for (i = 0; i < quant_final; i++) {
+            printf("Digite o nome do produto %d: ", i+1);
+            scanf("%s", produto[i]);
+        }
+    } else {
+        printf("Opção inválida.\n");
+    }
+    } while (escolha == 1 || 2);
     return 0;
 }
