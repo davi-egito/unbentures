26930aac150d037580698f5d6c4a77bb7d9e8b3d
Davi do Egito <66625452+zucomania@users.noreply.github.com>
2024-12-10 07:44:38 -0300

    Comments added

--- EDA1/EDA_P1/24.1/p1(24.1)_Q3.c
+++ EDA1/EDA_P1/24.1/p1(24.1)_Q3.c
@@ -5,9 +5,10 @@
 char *Q3(int *M, char *NC) {
     char *code = (char*) malloc(9 * sizeof(char));
     for (int i = 0; i < 9; i++) {
-        code[i] = NC[M[i]];
+        code[i] = NC[M[i]]; //o i vai aumentando gradativamente e com isso m vai pegando o mesmo índice
+        //do número de matrícula [i=0 / m[0] = 2/ NC[2] = 'v' / code[0] = 'v']
         if (code[i] == ' ') {
-            code[i] = '-'; 
+            code[i] = '-'; //substitui espaço por hífen
         }
     }
     return code;
@@ -17,10 +18,10 @@ char *Q3(int *M, char *NC) {
 // NC = SEU NOME COMPLETO EM LETRAS MINUSCULAS
 int main() {
     setlocale(LC_ALL, "Portuguese_Brazil");
-    int m[] = {2,2,2,0,0,6,1,7,8};
-    char nc[] = "thales henrique euflauzino dos santos";
+    int m[] = {2,3,1,0,3,0,4,2,1};
+    char nc[] = "davi marques do egito coelho";
     char *result = Q3(m, nc);
-    printf("Resultado: %s\n", result);
+    printf("Resultado: %s\n", result); 
     free(result); 
     return 0;
 }
